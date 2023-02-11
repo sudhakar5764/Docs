5f0045ad1f571f2f9a8695551d877d9096b49ae8
sudhakar5764 <41122256+sudhakar5764@users.noreply.github.com>
2023-02-11 15:26:39 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+arr2 = np.array([2,3,4,6,7])
+
+x = arr + arr2
+
+
+print(x)
+
+
+# In[ ]:
+
+
